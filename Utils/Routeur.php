2027135bf480c2da422e919b8065b6925c6d6697
Sam <samyr.rakoto@gmail.com>
2020-03-12 23:38:16 +0100

    start of a better controller, not done but to be continued

--- Utils/Routeur.php
+++ Utils/Routeur.php
@@ -2,58 +2,90 @@
 namespace Utils;
 
 class Routeur {
-	private $get;
+	private $path;
 
-	function __construct($get) {
-		$this->get = $get;
+	function __construct($path) {
+		$this->path = $path;
 	}
 
-	public function route() {
-		if (isset($this->get['id'])) {
-			return ['getUserById', $this->get['id']];
+	public function route(){
+        switch ($this->path[0]){
+            case 'app':
+                return $this->routeApp($this->path);
+                break;
+            case 'api':
+                return $this->routeApi($this->path);;
+                break;
+            default:
+                return $this->toggle404();
+                break;
+        }
+    }
+
+	private function routeApp($path) {
+	    if ($path[1] == "id"){
+            if(array_key_exists(2, $path)){
+                if (is_numeric($path[2]))
+                {
+                    return ['getUserById',$path[2]];
+                }
+            }
+        } else if($path[1] == "calculator") {
+	        return ['calculator'];
+        }
+		/*if (isset($this->$path['id'])) {
+			return ['$pathUserById', $this->$path['id']];
 		}
-		else if (isset($this->get['calculator'])) {
+		else if (isset($this->$path['calculator'])) {
 			return ['calculator'];
 		}
-		else if (isset($this->get['usercalculator'])) {
+		else if (isset($this->$path['usercalculator'])) {
 			return ['userCalculator'];
 		}
-		else if (isset($this->get['login'])) {
+		else if (isset($this->$path['login'])) {
 			return ['login'];
 		}
-		else if (isset($this->get['account'])) {
+		else if (isset($this->$path['account'])) {
 			return ['account'];
 		}
-		else if (isset($this->get['logout'])) {
+		else if (isset($this->$path['logout'])) {
 			return ['logout'];
 		}
-		else if (isset($this->get['dashboard'])) {
+		else if (isset($this->$path['dashboard'])) {
 			return ['dashboard'];
 		}
-		else if (isset($this->get['profile'])) {
+		else if (isset($this->$path['profile'])) {
 			return ['profile'];
 		}
-		else if (isset($this->get['changedata'])) {
+		else if (isset($this->$path['changedata'])) {
 			return ['changeData'];
 		}
-		else if (isset($this->get['savedata'])) {
+		else if (isset($this->$path['savedata'])) {
 			return ['saveData'];
 		}
-		else if (isset($this->get['newaccount'])) {
+		else if (isset($this->$path['newaccount'])) {
 			return ['newAccount'];
 		}
-		else if (isset($this->get['createaccount'])) {
+		else if (isset($this->$path['createaccount'])) {
 			return ['createAccount'];
 		}
-		else if (isset($this->get['api']) & isset($this->get['id'])) {
-			return ['getUserById', $this->get['id']];
+		else if (isset($this->$path['api']) & isset($this->$path['id'])) {
+			return ['$pathUserById', $this->$path['id']];
 		}
-		else if (isset($this->get['api']) & isset($this->get['bmr'])) {
+		else if (isset($this->$path['api']) & isset($this->$path['bmr'])) {
 			return ['calculateBmr'];
 		}
 		// If no parameter is set then go to the homepage
 		else {
 			return ['homepage'];
-		}
+		}*/
 	}
+
+	private function routeApi(){
+
+    }
+
+    private function toggle404(){
+        return '404';
+    }
 }
--- index.php
+++ index.php
@@ -2,25 +2,21 @@
     require_once ('Controller/UserController.php');
     require_once('Controller/api/UserApiController.php');
     require_once('Utils/Routeur.php');
+    require_once('Utils/UriParser.php');
 	use Controller\UserController;
 	use Utils\Routeur;
+	use Utils\UriParser;
 	use Controller\api\UserApiController;
 
 	session_start();
 
-	$uri = $_SERVER['REQUEST_URI'];
-	$uri = trim($uri, "/");
-	$path = explode("/", $uri);
-	$path = array_splice($path, 2);
-	$path = array_flip($path);
-
+	$path = UriParser::parseUri();
 	$routeur = new Routeur($path);
 	$action = $routeur->route(); // $action is an array containing a method and the parameters
 	$method = $action[0]; // Contains the method
 	$param = isset($action[1]) ? $action[1] : "";
-
-	//var_dump($path);
-	//exit;
+	var_dump($path);
+	var_dump($action);exit;
 
 	// Goes through API Controller
 	if (array_key_exists("api", $path)) {
