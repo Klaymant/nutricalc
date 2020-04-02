6df3eb2b4cc490d745338cbdec72162d9ca76db0
Kyromane <c.gauthier94250@gmail.com>
2020-04-02 16:36:31 +0200

    Solved URI problems when trying to access menu elements from the addTraining page by using an absolute path such as 'http://localhost/nutricalc/index.php/app/...'

--- View/navView.php
+++ View/navView.php
@@ -2,13 +2,13 @@
 	<ul>
 		<?php
 			if (isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
-				echo '<li><a href="calculator">Calculator</a></li>';
-				echo '<li><a href="login">Login</a></li>';
+				echo '<li><a href="http://localhost/nutricalc/index.php/app/calculator">Calculator</a></li>';
+				echo '<li><a href="http://localhost/nutricalc/index.php/app/login">Login</a></li>';
 			}
 			if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
-				echo '<li><a href="dashboard">Dashboard</a></li>';
-				echo '<li><a href="settings">Settings</a></li>';
-				echo '<li><a href="logout">Logout</a></li>';
+				echo '<li><a href="http://localhost/nutricalc/index.php/app/dashboard">Dashboard</a></li>';
+				echo '<li><a href="http://localhost/nutricalc/index.php/app/settings">Settings</a></li>';
+				echo '<li><a href="http://localhost/nutricalc/index.php/app/logout">Logout</a></li>';
 			}
 		?>
 	</ul>
