8bd5ce0bcc0797279a22871f26e98bd64a300309
Kyromane <c.gauthier94250@gmail.com>
2020-04-11 17:02:49 +0200

    Added an icon for dashboard in the menu and putting white icons instead of black.

--- View/navView.php
+++ View/navView.php
@@ -5,13 +5,13 @@
 			use Config\Path;
 
 			if (isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
-				echo '<li><a href="' . PATH::KERNEL . 'app/calculator"><img src="http://localhost/nutricalc/Public/Assets/img/calculator.png" />Calculator</a></li>';
-				echo '<li id="login"><a href="' . PATH::KERNEL . 'app/login"><img src="http://localhost/nutricalc/Public/Assets/img/switch_icon.png" />Login</a></li>';
+				echo '<li><a href="' . PATH::KERNEL . 'app/calculator"><img src="http://localhost/nutricalc/Public/Assets/img/calculator_white_icon.png" />Calculator</a></li>';
+				echo '<li id="login"><a href="' . PATH::KERNEL . 'app/login"><img src="http://localhost/nutricalc/Public/Assets/img/switch_white_icon.png" />Login</a></li>';
 			}
 			if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
-				echo '<li><a href="' . PATH::KERNEL . 'app/dashboard">Dashboard</a></li>';
-				echo '<li><a href="' . PATH::KERNEL . 'app/settings"><img src="http://localhost/nutricalc/Public/Assets/img/settings_icon.png" />Settings</a></li>';
-				echo '<li id="logout"><a href="' . PATH::KERNEL . 'app/logout"><img src="http://localhost/nutricalc/Public/Assets/img/switch_icon.png" />Logout</a></li>';
+				echo '<li><a href="' . PATH::KERNEL . 'app/dashboard"><img src="http://localhost/nutricalc/Public/Assets/img/dashboard_white_icon.png" />Dashboard</a></li>';
+				echo '<li><a href="' . PATH::KERNEL . 'app/settings"><img src="http://localhost/nutricalc/Public/Assets/img/settings_white_icon.png" />Settings</a></li>';
+				echo '<li id="logout"><a href="' . PATH::KERNEL . 'app/logout"><img src="http://localhost/nutricalc/Public/Assets/img/switch_white_icon.png" />Logout</a></li>';
 			}
 		?>
 	</ul>
